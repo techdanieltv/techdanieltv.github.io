de2df52a1c633a6bef0e3a6dac7aa936136f2156
Daniel Galeazzo <da.galaezzo@ticinumaerospace.com>
2025-01-13 03:21:33 +0100

    ...

--- run.sh
+++ run.sh
@@ -1,15 +1,6 @@
 rm -r assets *.js *.html *.css
 cp -r ../GestioneFinanze/www/browser/* .
-echo '<!-- 404.html -->
-<!DOCTYPE html>
-<html>
-  <head>
-    <meta http-equiv="refresh" content="0; URL=/" />
-  </head>
-  <body>
-  </body>
-</html>
-' > 404.html
+echo 'ciao' > index.html
 git add .
 git commit -a -m "..."
 git push
